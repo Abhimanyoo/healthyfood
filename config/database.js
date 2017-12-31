72c473a7c91fb81b8f2d2d40ea24bdbca1253bb7
Stephen Nyamweya <stephen.nyamweya.o@gmail.com>
2017-12-31 16:17:29 +0300

    Update database.js
    changes to db config

--- config/database.js
+++ config/database.js
@@ -1,4 +1,4 @@
-var url = 'mongodb://127.0.0.1/healthyfood';
+var url = 'mongodb://healthyfood-app:r3d33m3d@ds121575.mlab.com:21575/healthyfood'; //var url = 'mongodb://127.0.0.1/healthyfood';
 module.exports = {
   'url': url,
 };
